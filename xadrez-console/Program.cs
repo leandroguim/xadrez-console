a5bfc2d0ae4cfcef4cbd2caf2ce85a5cbca56c95
Leandro Guimaraes <leandroguim@hotmail.com>
2023-08-07 17:13:46 -0300

    Criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,17 +4,22 @@ using xadrez_console;
 using xadrez;
 using tabuleiro.Enums;
 
-try
-{
-    Tabuleiro tab = new Tabuleiro(8, 8);
+//try
+//{
+//    Tabuleiro tab = new Tabuleiro(8, 8);
 
-    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+//    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+//    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+//    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-    Tela.ImprimirTabuleiro(tab);
-}
-catch (TabuleiroException e)
-{
-    Console.WriteLine(e.Message);   
-}
+//    Tela.ImprimirTabuleiro(tab);
+//}
+//catch (TabuleiroException e)
+//{
+//    Console.WriteLine(e.Message);   
+//}
+
+PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+Console.WriteLine(pos);
+Console.WriteLine(pos.ToPosicao());
